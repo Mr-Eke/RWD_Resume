baeb1f99c7eee73df2fe52bb32f81a26a225b662
Mr-Eke <c.eke@alustudent.com>
2025-02-20 17:43:07 +0200

    Fix: Linked EmailJs API

--- script/main.js
+++ script/main.js
@@ -25,3 +25,26 @@ window.addEventListener('scroll', () => {
     window.scrollY > 150 ? header.classList.add("scroll-header")
         : header.classList.remove("scroll-header")
 });
+
+
+/* ====================== EmailJs API Integration =====================*/
+const contactForm = document.getElementById("contact-form"),
+    contactMessage = document.getElementById("contact-message")
+
+const sendEmail = (event) => {
+    event.preventDefault()
+
+    // SeviceID - TemplateID - #Form - PublicKey
+    emailjs.sendForm("service_f0n72lt", "template_4cs1frb", "#contact-form", "eVzV4a0lXyF737Dw4")
+        // Show message progress & take it off after 5sec
+        .then(() => {
+            contactMessage.textContent = "Your message has been sent! ✅"
+            setTimeout(() => { contactMessage.textContent = "" }, 5000)
+
+            contactForm.reset() // Clear Form
+        }, () => {
+            contactMessage.textContent = "❌ Message failed to send!"
+        })
+
+}
+contactForm.addEventListener("submit", sendEmail)
